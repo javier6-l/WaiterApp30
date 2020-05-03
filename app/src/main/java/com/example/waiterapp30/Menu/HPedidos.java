859d90e02aa025486a12e2bc21eca4efe0dafa79
zarate06 <zarate0616@hotmail.com>
2020-05-03 16:15:11 -0500

    hpedidos

--- app/src/main/java/com/example/waiterapp30/Menu/HPedidos.java
+++ app/src/main/java/com/example/waiterapp30/Menu/HPedidos.java
@@ -1,16 +1,21 @@
 package com.example.waiterapp30.Menu;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.waiterapp30.R;
 
 public class HPedidos extends AppCompatActivity {
+    GridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_h_pedidos);
+
+        gridView = findViewById(R.id.grid_view);
+
     }
 }
