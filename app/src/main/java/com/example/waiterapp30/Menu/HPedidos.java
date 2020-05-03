7a8f3a56347c10c9df2a2201221208a88a811070
zarate06 <zarate0616@hotmail.com>
2020-05-02 21:05:44 -0500

    HPedidos

---
+++ app/src/main/java/com/example/waiterapp30/Menu/HPedidos.java
@@ -0,0 +1,16 @@
+package com.example.waiterapp30.Menu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.waiterapp30.R;
+
+public class HPedidos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_h_pedidos);
+    }
+}
